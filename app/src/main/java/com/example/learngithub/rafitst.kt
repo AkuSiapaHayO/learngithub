dfd090af1e7353fed8b0c1d274fb3cc82676fa1f
oxqlion <rafinaya1127@gmail.com>
2024-10-22 11:26:40 +0700

    update: added new rafitst file

---
+++ app/src/main/java/com/example/learngithub/rafitst.kt
@@ -0,0 +1,9 @@
+package com.example.learngithub
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Baru() {
+    Text("Halo update baru dari branch 'rafi'.")
+}
